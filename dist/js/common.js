3ddb2def57e2a723a85040dd604412a9d68268bd
Yurii Yurakh <yurayurakh@gmail.com>
2018-06-27 13:25:13 +0300

    release2

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,12 +1,59 @@
-var $W = $(window),
-    $D = $(document),
-    $B = $("body"),
+var $D = $(document),
     $HB = $("html, body");
 
 (function () {
 
     $D.ready(function() {
 
+        youtubePlugIn();
+
+        function youtubePlugIn() {
+            var tag = document.createElement('script');
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+            window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+
+            function onYouTubeIframeAPIReady() {}
+        }
+
+        function popBoxStart(popId){
+            var $thisPopbox = $( popId ),
+                $popYoutube = $thisPopbox.find(".js-youtube-block"),
+                youtubeSource = $popYoutube.data("yt-source"),
+                widthPlayer = 660,
+                heightPlayer = 370,
+                youtubePlayer;
+
+            if(screen.width < 768){
+                widthPlayer = 320;
+                heightPlayer = 180;
+            }
+
+            youtubePlayer = new YT.Player( $popYoutube[0], {
+                videoId: youtubeSource, // YouTube Video ID
+                width: widthPlayer,               // Player width (in px)
+                height: heightPlayer,              // Player height (in px)
+                playerVars: {
+                    autoplay: 0,        // Auto-play the video on load
+                    controls: 1,        // Show pause/play buttons in player
+                    showinfo: 0,        // Hide the video title
+                    modestbranding: 0,  // Hide the Youtube Logo
+                    playlist: youtubeSource,
+                    loop: 0,            // Run the video in a loop
+                    rel: 0,
+                    fs: 1,              // Show the full screen button
+                    cc_load_policy: 0,  // Hide closed captions
+                    iv_load_policy: 3,  // Hide the Video Annotations
+                    autohide: 1         // Hide video controls when playing
+                }
+            });
+        }
+
+        setTimeout(function () {
+            popBoxStart( "#block-youtube");
+        }, 1000);
 
         $('#owl-reviews').owlCarousel({
             loop: true,
@@ -53,65 +100,6 @@ var $W = $(window),
             $("#"+dataTab).fadeIn().css("display", "flex");
         });
 
-        youtubePlugIn();
-
-        function youtubePlugIn() {
-            var tag = document.createElement('script');
-            tag.src = "https://www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-            window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-
-            function onYouTubeIframeAPIReady() {
-                //console.log("onYouTubeIframeAPI Ready");
-            }
-        }
-
-        function popBoxStart(popId){
-            var $thisPopbox = $( popId ),
-                $popYoutube = $thisPopbox.find(".js-youtube-block"),
-                youtubeSource = $popYoutube.data("yt-source"),
-                widthPlayer = 660,
-                heightPlayer = 370,
-                youtubePlayer;
-
-            if(screen.width < 768){
-                widthPlayer = 320;
-                heightPlayer = 180;
-            }
-
-            youtubePlayer = new YT.Player( $popYoutube[0], {
-                videoId: youtubeSource, // YouTube Video ID
-                width: widthPlayer,               // Player width (in px)
-                height: heightPlayer,              // Player height (in px)
-                playerVars: {
-                    autoplay: 0,        // Auto-play the video on load
-                    controls: 1,        // Show pause/play buttons in player
-                    showinfo: 0,        // Hide the video title
-                    modestbranding: 0,  // Hide the Youtube Logo
-                    playlist: youtubeSource,
-                    loop: 0,            // Run the video in a loop
-                    rel: 0,
-                    fs: 1,              // Show the full screen button
-                    cc_load_policy: 0,  // Hide closed captions
-                    iv_load_policy: 3,  // Hide the Video Annotations
-                    autohide: 1         // Hide video controls when playing
-                }
-            });
-
-            function stopPlay() {
-                if ( youtubePlayer ) {
-                    youtubePlayer.destroy();
-                    youtubePlayer = "";
-                }
-            }
-        }
-
-        setTimeout(function () {
-            popBoxStart( "#block-youtube");
-        }, 1000);
-
         // Popup
         $(".js-modal").click(function () {
 
@@ -138,15 +126,13 @@ var $W = $(window),
                 target = $('[data-scroll-target="' + $this.attr('data-scroll') + '"]');
 
             if(target.length) {
-                $('html,body').stop().animate({ scrollTop: target.offset().top }, 1000);
+                $HB.stop().animate({ scrollTop: target.offset().top }, 1000);
             }
         });
 
         // Phone mask
         $(".tel").mask("+38 (099) 999-99-99");
-
     });
-
 })();
 
 /*
